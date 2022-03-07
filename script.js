2de046a1a9b3ebaa8220e63163550ef0eb12117f
kezraFlyhigh <97551724+kezraFlyhigh@users.noreply.github.com>
2022-03-07 15:21:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,20 +14,25 @@ const score0 = document.querySelector(`#score--0`);
 const score1 = document.getElementById(`score--1`);
 const name = document.querySelector(`.name`);
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
-alert(`Poenget med spillet er å ikke kaste 1 på terningen.`);
-alert(
-  `Kaster du 1 mister du current poengscore og det blir neste spillers tur.`
-);
-alert(
-  `Du kan lagre summen du har kastet deg til
- ved å trykke "Hold"
- da blir det neste spiller sin tur.`
-);
+// Initial values
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  dice.classList.add('hidden');
+  score0.textContent = currentScore;
+  score1.textContent = currentScore;
+  current0El.textContent = currentScore;
+  current1El.textContent = currentScore;
+};
+init();
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -38,54 +43,61 @@ const switchPlayer = function () {
   player1El.classList.toggle(`player--active`);
 };
 
-// Starting conditions
-score0.textContent = 0;
-score1.textContent = 0;
-dice.classList.add('hidden');
-
 // Rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
   if (playing) {
-    // .1 Generating a random number
+    // Input number from 1-6
     let diceGen = Math.trunc(Math.random() * 6 + 1);
-    // Unless it's a 1, update score and show correct dice img
+    // Show dice img depending on result above
     dice.classList.remove(`hidden`);
     dice.src = `dice-${diceGen}.png`;
-    // .2 Sum score + generated number
-    
-    // .3 If hitting a 1, reset score and move on to next player
+    // if not a 1, do this;
     if (diceGen !== 1) {
       // Add dice to current score
       currentScore += diceGen;
-      // Variable of who gets their score updated
+      // Variable of ${who} gets their score updated, and update it
       document.querySelector(`#current--${activePlayer}`).textContent =
         currentScore;
 
-      //Predefined how is getting their score updated (player1)
-      //current0El.textContent = currentScore; //Change later
+      // If a 1 gets rolled, do function which is writen further up
     } else switchPlayer();
-    // Switch to next player
+    // Which switches to next player
   }
 });
-/*
-btnHold.addEventListener(`click`, function () {
-  document.querySelector(`#score--${activePlayer}`).textContent = scores[
-    `${activePlayer}`
-  ] += currentScore;
 
-  currentScore = 0;
-  document.querySelector(`#current--${activePlayer}`).textContent =
-    currentScore;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle(`player--active`);
-  player1El.classList.toggle(`player--active`);
-  if (score[activePlayer] > 100) {
-    document.querySelector(
-      `#current--${activePlayer}`
-    ).textContent = `You won the game`;
+// Holding score function
+btnHold.addEventListener(`click`, function () {
+  // if playing makes is so you can only play if there hasn't been decleared a winner
+  if (playing) {
+    // Sums "the score" + currentscore
+    scores[`${activePlayer}`] += currentScore;
+    // Shows the new score
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[`${activePlayer}`];
+    dice.classList.add('hidden');
+
+    // if "the score" is above 100 do this;
+    if (scores[`${activePlayer}`] > 100) {
+      // Hide the dice IMG
+
+      // Stop the game
+      playing = false;
+      // Remove "current player" highlight
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+      document
+        // Add "winner" background
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+
+      // If "the score" hasn't reached 100, switch player through function further up
+    } else switchPlayer();
   }
 });
-*/
+
+/*
+// This is how the instructor wrote it;
 btnHold.addEventListener(`click`, function () {
   if (playing) {
     scores[activePlayer] += currentScore;
@@ -105,56 +117,10 @@ btnHold.addEventListener(`click`, function () {
     } else switchPlayer();
   }
 });
-
-btnNew.addEventListener(`click`, function () {
-  //reset all scores, current and imagery
-  // let player 1 beging rolling
-  currentScore = 0;
-  activePlayer = 0;
-  // Remove dice and update scores to 0
-  dice.classList.add('hidden');
-  score0.textContent = currentScore;
-  score1.textContent = currentScore;
-  current0El.textContent = currentScore;
-  current1El.textContent = currentScore;
-  // remove "winner" background, and reset player 1 for beginning
-  player0El.classList.add(`player--active`);
-  player1El.classList.remove(`player--active`);
-  player0El.classList.remove(`player--winner`);
-  player1El.classList.remove(`player--winner`);
-  // Remove values in Scores and reinput 1
-  scores.pop();
-  scores.pop();
-  scores.push(0);
-  scores.push(0);
-  // Let playing commens
-  playing = true;
-});
-
-/*
-// Above as a function ;
-const init = function () {
-  const scores = [0, 0];
-  let currentScore = 0;
-  let activePlayer = 0;
-  let playing = true;
-  // Remove dice and update scores to 0
-  dice.classList.add('hidden');
-  score0.textContent = currentScore;
-  score1.textContent = currentScore;
-  current0El.textContent = currentScore;
-  current1El.textContent = currentScore;
-  // remove "winner" background, and reset player 1 for beginning
-  document.querySelector(`.player--0`).classList.add(`player--active`);
-  document.querySelector(`.player--1`).classList.remove(`player--active`);
-  document.querySelector(`.player--0`).classList.remove(`player--winner`);
-  document.querySelector(`.player--1`).classList.remove(`player--winner`);
-  // Remove values in Scores and reinput 1
-  scores.pop();
-  scores.pop();
-  scores.push(0);
-  scores.push(0);
-  // Let playing commens
-  playing = true;
-};
 */
+
+// If "new game" button is clicled
+btnNew.addEventListener(`click`, function () {
+  // Run initial funtion writen above
+  init();
+});
